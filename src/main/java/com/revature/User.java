d4d8a20d4a132c534ea5073b5f0c9d6c5932665f
MolSnoo <molsno.vivian@gmail.com>
2022-03-07 11:08:55 -0500

    Added User class

---
+++ src/main/java/com/revature/User.java
@@ -0,0 +1,57 @@
+package com.revature;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name="User")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private int id;
+    @Column(name="name")
+    private String name;
+    @Column(name="email")
+    private String email;
+    @Column(name="type")
+    private String type;
+
+    public User(int id, String name, String email, String type) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
