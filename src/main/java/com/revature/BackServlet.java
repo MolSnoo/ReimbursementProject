67f9e3624f44d9b09aa41a9006efb459ec074682
michaelbutera <michael213@revature.net>
2022-03-09 00:56:36 -0500

    centered homepage buttons & added the start of employee's view info page functionality

---
+++ src/main/java/com/revature/BackServlet.java
@@ -0,0 +1,39 @@
+package com.revature;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BackServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        // Ensure that the page isn't cached so that the user can't access the wrong account type after logging out.
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+        response.setHeader("Pragma", "no-cache");   // HTTP 1.0
+        response.setDateHeader("Expires", 0);   // Proxies
+        PrintWriter out = response.getWriter();
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            String userType = (String) session.getAttribute("user_type");
+            if (userType.equals("EMPLOYEE")) {
+                RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ehomepage.html");
+                dispatcher.forward(request, response);
+            }
+            else if (userType.equals("MANAGER")) {
+                RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/mhomepage.html");
+                dispatcher.forward(request, response);
+            }
+        }
+        else {
+            response.sendRedirect(request.getContextPath() + "/loginpage.html");
+        }
+        out.close();
+    }
+}
---
+++ src/main/java/com/revature/ViewInfoServlet.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class ViewInfoServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        //response.sendRedirect(request.getContextPath() + "/ehomepage.html");
+        getServletContext().getRequestDispatcher("/viewinfo.html");
+    }
+}
